fce04e0f2f33f54c39d20fa3744181516118cfa3
SnehaAshanapally <SnehaAshanapally@my.unt.edu>
2024-12-02 21:41:04 -0600

    Update home.py
    Basic user interface has been changed

--- home.py
+++ home.py
@@ -57,22 +57,22 @@ def goBack():
 def basicPageSetup(pageNo):
     global left_frame, right_frame, heading
 
-    back_img = tk.PhotoImage(file= r"C:\Users\hp\Documents\GitHub\Face-Recognition-For-Criminal-Detection-GUi\img\back.png")
-    back_button = tk.Button(pages[pageNo], image=back_img, bg="#3E3B3C", bd=0, highlightthickness=0,
-           activebackground="#3E3B3C", command=goBack)
+    back_img = tk.PhotoImage(file= r"E:\FinalYearProjectDetails\MajorprojectFacialRecognition\Facial-Recognition-for-Crime-Detection\img\back.png")
+    back_button = tk.Button(pages[pageNo], image=back_img, bg="#e1e8df", bd=0, highlightthickness=0,
+           activebackground="#e1e8df", command=goBack)
     back_button.image = back_img
-    back_button.place(x=10, y=10)
+    back_button.place(x=2, y=2)
 
-    heading = tk.Label(pages[pageNo], fg="white", bg="#3E3B3C", font="Arial 20 bold", pady=10)
+    heading = tk.Label(pages[pageNo], fg="black", bg="#e1e8df", font="Arial 20 bold", pady=10)
     heading.pack()
 
-    content = tk.Frame(pages[pageNo], bg="#3E3B3C", pady=20)
+    content = tk.Frame(pages[pageNo], bg="#e1e8df", pady=20)
     content.pack(expand="true", fill="both")
 
-    left_frame = tk.Frame(content, bg="#3E3B3C")
+    left_frame = tk.Frame(content, bg="#e1e8df")
     left_frame.grid(row=0, column=0, sticky="nsew")
 
-    right_frame = tk.LabelFrame(content, text="Detected Criminals", bg="#3E3B3C", font="Arial 20 bold", bd=4,
+    right_frame = tk.LabelFrame(content, text="Detected Criminals", bg="#e1e8df", font="Arial 20 bold", bd=4,
                              foreground="#2ea3ef", labelanchor="n")
     right_frame.grid(row=0, column=1, sticky="nsew", padx=20, pady=20)
 
@@ -80,7 +80,13 @@ def basicPageSetup(pageNo):
     content.grid_columnconfigure(1, weight=1, uniform="group1")
     content.grid_rowconfigure(0, weight=1)
 
+ # Add logo to the left frame at the bottom-left corner
+    logo = tk.PhotoImage(file=r"E:\FinalYearProjectDetails\MajorprojectFacialRecognition\Facial-Recognition-for-Crime-Detection\colours.png")
+    logo_label = tk.Label(left_frame, image=logo, bg="#e1e8df", bd=0)
+    logo_label.pack(side='bottom', padx=10, pady=10, anchor='sw')  # Adjust padx and pady values as needed
 
+
+    
 def showImage(frame, img_size):
     global img_label, left_frame
 
